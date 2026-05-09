4f0a01bebaa725ddaa2bfb787c6b953345504d48
Miguel Angel Villena Bustamante <migue.villena.b@utec.edu.pe>
2026-05-09 15:10:11 -0500

    Inicial commit

---
+++ src/main/java/com/example/p_flayawaytravel/PFlayAwayTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.p_flayawaytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PFlayAwayTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PFlayAwayTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/p_flayawaytravel/PFlayAwayTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p_flayawaytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PFlayAwayTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
